723d4e046ee9bb213298ac19104db09f1504233a
wyllman <wyllman@gmail.com>
2014-06-04 18:50:47 +0100

    primera version del generador de enemigos.

--- Assets/Scripts/EnemyCreator.cs
+++ Assets/Scripts/EnemyCreator.cs
@@ -2,14 +2,62 @@
 using System.Collections;
 
 public class EnemyCreator : MonoBehaviour {
+	// Prefab del objeto enemigo
+	public GameObject enemyPrefab;
 
+	// Definir las posiciones de generación para
+	// los enemigos.
+	private Vector3 guardPosB01 = new Vector3 (5, 0, 5);
+	private Vector3 guardPosB02 = new Vector3 (-5, 0, 5);
+
+	private Vector3 guardPosF01 = new Vector3 (10, 0, 10);
+	private Vector3 guardPosF02 = new Vector3 (0, 0, 10);
+	private Vector3 guardPosF03 = new Vector3 (-10, 0, 10);
+
+	// Referencias a los enemigos generados actualmente.
+	private GameObject guardEnemyB01 = null;
+	private GameObject guardEnemyB02 = null;
+
+	private GameObject guardEnemyF01 = null;
+	private GameObject guardEnemyF02 = null;
+	private GameObject guardEnemyF03 = null;
+
+	// Contadores de tiempo de espera para una nueva
+	// generación de enemigos.
+	private const float IDLE_TIME = 3.0f;
+	private float timeIdleB01 = IDLE_TIME;
+	private float timeIdleB02 = IDLE_TIME;
+
+	private float timeIdleF01 = IDLE_TIME;
+	private float timeIdleF02 = IDLE_TIME;
+	private float timeIdleF03 = IDLE_TIME;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		checkEnemiesPos (ref guardEnemyB01, guardPosB01, ref timeIdleB01);
+		checkEnemiesPos (ref guardEnemyB02, guardPosB02, ref timeIdleB02);
+
+		checkEnemiesPos (ref guardEnemyF01, guardPosF01, ref timeIdleF01);
+		checkEnemiesPos (ref guardEnemyF02, guardPosF02, ref timeIdleF02);
+		checkEnemiesPos (ref guardEnemyF03, guardPosF03, ref timeIdleF03);
+	}
+
+	void checkEnemiesPos (ref GameObject enemyObj, Vector3 generatePos, ref float timeIdle) {
+		if (enemyObj == null) {
+		  if (timeIdle > 0) {
+			timeIdle -= Time.deltaTime;
+		  } else {
+			timeIdle = IDLE_TIME;
+			enemyObj = GameObject.Instantiate (
+				enemyPrefab,
+				generatePos,
+				Quaternion.identity) as GameObject;
+		  }
+		}
+		
 	}
 }
