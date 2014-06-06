2a77f70259201e60af2cbef2d24b7b74ed3ab77e
Blaise <Blaiserg@gmail.com>
2014-06-06 14:11:42 +0100

    Empezando
    Actualizando el nombre del componente y el tipo de gameObject asi como
    el script asociado a este

---
+++ Assets/Scripts/ChangeFloor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeFloor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/*
+			Se dispara cuando otro collider entra en la zona del collider de un chanFloor
+	 */
+	void OnTriggerEnter(Collider other) {
+
+	}
+}
